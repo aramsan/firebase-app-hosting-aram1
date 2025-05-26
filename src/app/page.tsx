df0d6c99bb51927e38358bdbc44905356b02fd76
aram.mine <aram.mine@gaiax.com>
2025-05-26 17:45:18 +0900

    Test apphosting:secret on index page part 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          ENV STR : "{str}" SECRET STR : "{ secStr }"
+          ENV STR : &quot;{str}&ldquo; SECRET STR : &quot;{ secStr }&ldquo;
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
